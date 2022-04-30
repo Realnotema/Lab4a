e1420804836c5634a5c0b957ac3779fb1f539fbb
Realnotema <100119630+Realnotema@users.noreply.github.com>
2022-04-30 23:27:24 +0300

    Update process.c

--- process.c
+++ process.c
@@ -1,26 +1,26 @@
 #include <stdio.h>
 
 int addElement (Tree *tree, int key, int info) {
-        Node *temp = (Node *) calloc(1, sizeof(Node));                                                                                                                                                    
-        temp->key = key;                                                                                                                                                                                  
-        temp->info = info;                                                                                                                                                                                
-        if (tree->root == NULL) {                                                                                                                                                                         
-                tree->root = temp;                                                                                                                                                                        
-                return 0;                                                                                                                                                                                 
+        Node *temp = (Node *) calloc(1, sizeof(Node));
+        temp->key = key;
+        temp->info = info;
+        if (tree->root == NULL) {
+                tree->root = temp;
+                return 0;
         }
-        Node *ptr = tree->root;                                                                                                                                                                           
-        Node *par = NULL;                                                                                                                                                                                 
-        while (ptr != NULL) {                                                                                                                                                                             
-                par = ptr;                                                                                                                                                                                
-                if (temp->key < ptr->key)                                                                                                                                                                 
-                        ptr = ptr->left;                                                                                                                                                                  
-                else                                                                                                                                                                                      
-                        ptr = ptr->right;                                                                                                                                                                 
+        Node *ptr = tree->root;
+        Node *par = NULL;
+        while (ptr != NULL) {
+                par = ptr;
+                if (temp->key < ptr->key)
+                        ptr = ptr->left;
+                else
+                        ptr = ptr->right;
         }
-        if (temp->key < par->key)                                                                                                                                                                         
-                par->left = temp;                                                                                                                                                                         
-        else                                                                                                                                                                                              
-                par->right = temp;                                                                                                                                                                        
+        if (temp->key < par->key)
+                par->left = temp;
+        else
+                par->right = temp;
         temp->par = par;
-        return 0;                                                                                                                                                                                         
+        return 0;
 }
