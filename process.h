fe879cd3aed6646138d564968e75f0cbef6ac5db
Realnotema <100119630+Realnotema@users.noreply.github.com>
2022-06-04 12:33:18 +0300

    Update process.h

--- process.h
+++ process.h
@@ -5,11 +5,13 @@ int addElement (Tree *tree, int key, int info);
 Node *traversal (Tree *tree, Node *ptr);
 Node *special (Tree *tree);
 int findByKey (Tree *tree, int key);
-int delElement (Tree *tree, int key);
+int delElement (Tree *tree, Node *ptr, int key);
 int freeTree (Node *root);
 int printTree (Node *root, int p);
 int writeTree (FILE *fb, Tree *tree);
 int processWriteTree (FILE *fb, Node *root, int headKey, int p);
+int writeFrequency (FILE *fb, Tree *tree);
+int writeBinaryTree (FILE *fb, Tree *tree);
 int readTree (FILE *fb, Tree *tree);
 
 #endif
